26f912e4d32240d37a1a41918a2b44e60e7452ef
rossipedia <bryan.ross@docker.com>
2024-07-15 12:11:09 -0600

    add repro

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import mdx from '@mdx-js/rollup';
+import { vitePlugin as remix } from '@remix-run/dev';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
+    mdx(),
     remix({
       future: {
         v3_fetcherPersist: true,
